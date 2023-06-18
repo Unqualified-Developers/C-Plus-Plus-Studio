377caa668a8548438a87b6147c814055fd134e16
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2023-06-18 14:09:03 +0800

    Optimize.

--- C++ Studio/Form1.cs
+++ C++ Studio/Form1.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using ScintillaNET;
+using System.Diagnostics;
 
 namespace C___Studio
 {
@@ -249,7 +250,7 @@ namespace C___Studio
 
         public void Compile()
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            Process process = new Process();
             process.StartInfo.FileName = ".\\MinGW\\bin\\g++.exe";
             string filebc;
             try
@@ -296,7 +297,7 @@ namespace C___Studio
 
         public void NewEmpty()
         {
-            textBox1.Text = "int main() {\n    \n    return 0;\n}\n";
+            textBox1.Text = "int main() {\n    \n    return 0;\n}";
             openedAFile = false;
             file = null;
             Text = "C++ Studio";
@@ -304,7 +305,7 @@ namespace C___Studio
 
         public void NewCmdProgram()
         {
-            textBox1.Text = "#include <iostream>\nusing namespace std;\n\n// Main function.\nint main() {\n    cout << \"Hello World!\" << endl;\n    return 0;\n}\n";
+            textBox1.Text = "#include <iostream>\nusing namespace std;\n\n// Main function.\nint main() {\n    cout << \"Hello World!\" << endl;\n    return 0;\n}";
             openedAFile = false;
             file = null;
             Text = "C++ Studio";
@@ -312,7 +313,7 @@ namespace C___Studio
         
         public void NewWFProgram()
         {
-            textBox1.Text = "#include <windows.h>\r\n\r\n// This is where all the input to the window goes to.\r\nLRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {\r\n switch(Message) {\r\n        \r\n        // Upon destruction, tell the main thread to stop.\r\n        case WM_DESTROY: {\r\n            PostQuitMessage(0);\r\n            break;\r\n        }\r\n        \r\n        // All other messages (a lot of them) are processed using default procedures.\r\n        default:\r\n            return DefWindowProc(hwnd, Message, wParam, lParam);\r\n    }\r\n    return 0;\r\n}\r\n\r\n// The 'main' function of Win32 GUI programs: this is where execution starts.\r\nint WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {\r\n    WNDCLASSEX wc; // A properties struct of our window.\r\n    HWND hwnd; // A 'HANDLE', hence the H, or a pointer to our window.\r\n    MSG msg; // A temporary location for all messages.\r\n\r\n    // Zero out the struct and set the stuff we want to modify.\r\n    memset(&wc,0,sizeof(wc));\r\n    wc.cbSize = sizeof(WNDCLASSEX);\r\n    wc.lpfnWndProc = WndProc; // This is where we will send messages to.\r\n    wc.hInstance = hInstance;\r\n    wc.hCursor = LoadCursor(NULL, IDC_ARROW);\r\n    \r\n    // White, COLOR_WINDOW is just a #define for a system color.\r\n    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);\r\n    wc.lpszClassName = \"WindowClass\";\r\n    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Load a standard icon.\r\n    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION); // use the name \"A\" to use the project icon.\r\n\r\n    if(!RegisterClassEx(&wc)) {\r\n        MessageBox(NULL, \"Window Registration Failed!\",\"Error!\",MB_ICONEXCLAMATION|MB_OK);\r\n        return 0;\r\n    }\r\n\r\n    hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,\"WindowClass\",\"Caption\",WS_VISIBLE|WS_OVERLAPPEDWINDOW,\n        CW_USEDEFAULT, // x\r\n        CW_USEDEFAULT, // y\r\n        640, // width\r\n        480, // height\r\n        NULL,NULL,hInstance,NULL);\r\n\r\n    if(hwnd == NULL) {\r\n        MessageBox(NULL, \"Window Creation Failed!\",\"Error!\",MB_ICONEXCLAMATION|MB_OK);\r\n        return 0;\r\n    }\r\n\r\n    /*\r\n        This is the heart of our program where all input is processed and sent to WndProc. \r\n        Note that GetMessage blocks code flow until it receives something, so this loop will not produce unreasonably high CPU usage.        \r\n    */\r\n    while(GetMessage(&msg, NULL, 0, 0) > 0) { // If no error is received...\r\n        TranslateMessage(&msg); // Translate key codes to chars if present.\r\n        DispatchMessage(&msg); // Send it to WndProc.\r\n    }\r\n    return msg.wParam;\r\n}";
+            textBox1.Text = "#include <windows.h>\n\n// This is where all the input to the window goes to.\nLRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {\n switch(Message) {\n        \n        // Upon destruction, tell the main thread to stop.\n        case WM_DESTROY: {\n            PostQuitMessage(0);\n            break;\n        }\n        \n        // All other messages (a lot of them) are processed using default procedures.\n        default:\n            return DefWindowProc(hwnd, Message, wParam, lParam);\n    }\r\n    return 0;\n}\n\n// The 'main' function of Win32 GUI programs: this is where execution starts.\nint WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {\n    WNDCLASSEX wc; // A properties struct of our window.\n    HWND hwnd; // A 'HANDLE', hence the H, or a pointer to our window.\r\n    MSG msg; // A temporary location for all messages.\n\n    // Zero out the struct and set the stuff we want to modify.\n    memset(&wc,0,sizeof(wc));\n    wc.cbSize = sizeof(WNDCLASSEX);\n    wc.lpfnWndProc = WndProc; // This is where we will send messages to.\n    wc.hInstance = hInstance;\n    wc.hCursor = LoadCursor(NULL, IDC_ARROW);\n    \n    // White, COLOR_WINDOW is just a #define for a system color.\n    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);\n    wc.lpszClassName = \"WindowClass\";\n    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Load a standard icon.\n    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION); // use the name \"A\" to use the project icon.\n\n    if(!RegisterClassEx(&wc)) {\n        MessageBox(NULL, \"Window Registration Failed!\",\"Error!\",MB_ICONEXCLAMATION|MB_OK);\n        return 0;\n    }\n\n    hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,\"WindowClass\",\"Caption\",WS_VISIBLE|WS_OVERLAPPEDWINDOW,\n        CW_USEDEFAULT, // x\n        CW_USEDEFAULT, // y\n        640, // width\n        480, // height\n        NULL,NULL,hInstance,NULL);\n\n    if(hwnd == NULL) {\n        MessageBox(NULL, \"Window Creation Failed!\",\"Error!\",MB_ICONEXCLAMATION|MB_OK);\n        return 0;\n    }\n\n    /*\n        This is the heart of our program where all input is processed and sent to WndProc.\n        Note that GetMessage blocks code flow until it receives something, so this loop will not produce unreasonably high CPU usage.        \n    */\n    while(GetMessage(&msg, NULL, 0, 0) > 0) { // If no error is received...\n        TranslateMessage(&msg); // Translate key codes to chars if present.\n        DispatchMessage(&msg); // Send it to WndProc.\n    }\n    return msg.wParam;\n}";
             openedAFile = false;
             file = null;
             Text = "C++ Studio";
@@ -367,7 +368,7 @@ namespace C___Studio
 
         private void releasesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/Python-Object-Developers/C-Plus-Plus-Studio/releases");
+            Process.Start("https://github.com/Python-Object-Developers/C-Plus-Plus-Studio/releases");
         }
 
         private void windowsFormProgramToolStripMenuItem_Click(object sender, EventArgs e)
@@ -395,7 +396,7 @@ namespace C___Studio
 
         public void Debug() 
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            Process process = new Process();
             process.StartInfo.FileName = ".\\MinGW\\bin\\g++.exe";
             string filebc;
             try
