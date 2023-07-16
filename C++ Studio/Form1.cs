f0e8c38a9a8c44cc69388125e78106e5d4a9978c
CI47 <iamapig8@outlook.com>
2023-07-16 12:47:17 +0800

    Update Automatic prompt.

--- C++ Studio/Form1.cs
+++ C++ Studio/Form1.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Windows.Forms;
 using ScintillaNET;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace C___Studio
 {
@@ -59,7 +60,26 @@ namespace C___Studio
             textBox1.SetProperty("fold.compact", "1");
             textBox1.SetProperty("fold.comment", "1");
             textBox1.SetProperty("fold.preprocessor", "1");
-            textBox1.AutomaticFold = AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change;
+            textBox1.AutomaticFold = AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change;       
+        }
+
+        List<string> autoCompleteData = new List<string>
+        {
+            "asm", "auto", "break" ,"case" ,"catch" ,"char" ,"class" ,"const", "continue" ,"default" ,"delete", "do", "double" ,"dynamic_cast", "else", "enum", "explicit", "export", "extern" ,"float", "for", "friend" ,"goto", "if" ,"inline" ,"int" ,"long" ,"mutable" ,"namespace","new","operator" ,"private" ,"protected" ,"public" ,"register" ,"reinterpret_cast" ,"return" ,"short"
+            , "signed" ,"sizeof" ,"static" ,"static_cast" ,"struct", "switch","template" ,"throw" ,"try" ,"typedef" ,"typeid","typename", "union" ,"unsigned" ,"using", "virtual", "void" ,"volatile" ,"wchar_t" ,"while"
+        };
+
+        private void ShowAutoComplete()
+        {
+            // 获取当前光标位置之前的单词
+            int currentPos = textBox1.CurrentPosition;
+            int wordStartPos = textBox1.WordStartPosition(currentPos, true);
+            string currentWord = textBox1.GetTextRange(wordStartPos, currentPos - wordStartPos);
+
+            // 创建自动提示列表的字符串
+            string autoCompleteList = string.Join(" ", autoCompleteData);
+            // 显示自动提示列表
+            textBox1.AutoCShow(currentWord.Length, autoCompleteList);
         }
 
         public void OpenFile()
@@ -153,6 +173,7 @@ namespace C___Studio
         {
             needToSave = true;
             needToSaveBC = true;
+            ShowAutoComplete();
         }
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
